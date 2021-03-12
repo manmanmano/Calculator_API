7ee6ada0c0fdae35fef64a1a2e75d53167e5c453
Andre Tomingas <antomi@ttu.ee>
2021-03-12 12:45:23 +0200

    Added MaxOdd and Sum service

--- src/main/java/ee/taltech/calculator/service/MaxOddService.java
+++ src/main/java/ee/taltech/calculator/service/MaxOddService.java
@@ -1,4 +1,12 @@
 package ee.taltech.calculator.service;
 
+import java.util.List;
+
 public class MaxOddService {
+    public static Integer MaxOdd(List<Integer> numbers){
+        return numbers.stream()
+                .filter(x -> x % 2 != 0)
+                .max(Integer::compareTo)
+                .orElse(0);
+    }
 }
--- src/main/java/ee/taltech/calculator/service/SumService.java
+++ src/main/java/ee/taltech/calculator/service/SumService.java
@@ -1,4 +1,10 @@
 package ee.taltech.calculator.service;
 
+import java.util.List;
+
 public class SumService {
+    public static Integer Sum(List<Integer> numbers){
+        return numbers.stream()
+                .reduce(0, Integer::sum);
+    }
 }
