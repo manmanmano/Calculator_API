575c338172e82feba8af39d44d5a5459553534d0
Jan Markus Üprus <73178486+janyprus@users.noreply.github.com>
2021-03-12 12:29:25 +0200

    add logic for SignReversed

--- src/main/java/ee/taltech/calculator/service/SignReversedService.java
+++ src/main/java/ee/taltech/calculator/service/SignReversedService.java
@@ -1,4 +1,18 @@
 package ee.taltech.calculator.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SignReversedService {
+    public static List<Integer> SignReversed(List<Integer> numbers) {
+        if (numbers == null) {
+            throw new IllegalArgumentException("invalid input");
+        }
+        if (numbers.isEmpty()) {
+            return List.of();
+        }
+        return numbers.stream()
+                .map(integer -> -1 * integer)
+                .collect(Collectors.toList());
+    }
 }
