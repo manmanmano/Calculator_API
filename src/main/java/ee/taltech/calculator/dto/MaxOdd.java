24858416c2a23c8f5b712fa4cbb363879dfd5cea
Andre Tomingas <antomi@ttu.ee>
2021-03-12 12:34:31 +0200

    Added MaxOdd and Sum dto

--- src/main/java/ee/taltech/calculator/dto/MaxOdd.java
+++ src/main/java/ee/taltech/calculator/dto/MaxOdd.java
@@ -1,4 +1,13 @@
 package ee.taltech.calculator.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+
 public class MaxOdd {
+    private List<Integer> numbers;
 }
--- src/main/java/ee/taltech/calculator/dto/Sum.java
+++ src/main/java/ee/taltech/calculator/dto/Sum.java
@@ -1,4 +1,13 @@
 package ee.taltech.calculator.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+
 public class Sum {
+    private List<Integer> numbers;
 }
