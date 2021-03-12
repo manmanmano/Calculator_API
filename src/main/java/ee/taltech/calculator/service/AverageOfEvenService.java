750d1b84609507fd397a1e4e3ef2300c364982ce
Mariano D'Angelo <madang@ttu.ee>
2021-03-12 18:19:17 +0200

    beautified AverageOfEvenService

--- src/main/java/ee/taltech/calculator/service/AverageOfEvenService.java
+++ src/main/java/ee/taltech/calculator/service/AverageOfEvenService.java
@@ -7,33 +7,28 @@ import java.math.RoundingMode;
 
 public class AverageOfEvenService {
 
-    public static void main(String[] args) {
-        List<Integer> numbers = List.of(12, 5, 76, 11, 10);
-        System.out.println(averageOfEven(numbers));
-    }
-
     public static Double averageOfEven(List<Integer> numbers) {
         if (numbers == null) {
             return null;
         }
         if (numbers.isEmpty()) {
-            return Double.valueOf(0);
+            return (double) 0;
         }
         List<Integer> evens = numbers.stream()
                 .filter(integer -> integer % 2 == 0)
                 .collect(Collectors.toList());
         if (evens.isEmpty()) {
-            return Double.valueOf(0);
+            return (double) 0;
         }
-        Integer sum = evens.stream()
+        int sum = evens.stream()
                 .mapToInt(Integer::intValue)
                 .sum();
-        return roundDouble(((double) sum / evens.size()), 2);
+        return roundDouble((double) sum / evens.size());
     }
 
-    private static double roundDouble(double avg, int places) {
+    private static double roundDouble(double avg) {
         BigDecimal rounded = new BigDecimal(Double.toString(avg));
-        rounded = rounded.setScale(places, RoundingMode.HALF_UP);
+        rounded = rounded.setScale(2, RoundingMode.HALF_UP);
         return rounded.doubleValue();
     }
 }
