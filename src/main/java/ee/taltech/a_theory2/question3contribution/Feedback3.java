6392131338934ec83c0e357ee0d2cb5a0ab6bd86
Mariano D'Angelo <madang@ttu.ee>
2021-03-12 18:40:44 +0200

    Feedback3 done

--- src/main/java/ee/taltech/a_theory2/question3contribution/Feedback3.java
+++ src/main/java/ee/taltech/a_theory2/question3contribution/Feedback3.java
@@ -7,24 +7,28 @@ public class Feedback3 {
 
     // todo
     //  The course is coming to an end and I would like you to tell me how did it go
+    //  I really liked the course. The lectures were easy to follow and also fun to watch. This course has been my first
+    //  approach to OOP so I am glad I took it. I found it really hard in the beginning, but with some extra work
+    //  I managed to keep up and understand everything. I love streams... they are over powered.
 
     //todo A
     // On the scale 1 to 10 how useless or useful this course was with 1 being very useless and 10 being very useful?
-    //
+    // 10
+    // I learned Java so one of the most used languages, got into OOP, learned git, APIs, and SpringBoot.
 
     //todo B
     // Write some things you liked? At least 1, but you can write more.
-    //
+    // I liked pretty much everything. I really enjoyed making the projects and also doing the exercises. STREAMS 100%
     //
 
     //todo C
     // Write some things you didn't like? At least 1, but you can write more.
-    //
+    // In the beginning it was a bit hard to catch up since I didn't have any experience with OOP.
     //
 
     //todo D
     // Anything else to add? Comments? What to do differently next year? (optional)
-    //
+    // Everything was good.
     //
 
 }
