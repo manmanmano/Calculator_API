66dda2e3f998a8193e5fb6d2db2c6ecfebf07ac3
Mariano D'Angelo <madang@ttu.ee>
2021-03-12 12:54:07 +0200

    added logic to AverageOfService

--- src/main/java/ee/taltech/calculator/service/AverageOfEvenService.java
+++ src/main/java/ee/taltech/calculator/service/AverageOfEvenService.java
@@ -4,5 +4,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class AverageOfEvenService {
-
+    public static Double averageOfEven(List<Integer> numbers) {
+        if (numbers == null) {
+            return null;
+        }
+        if (numbers.isEmpty()) {
+            return Double.valueOf(0);
+        }
+        List<Integer> evens = numbers.stream()
+                .filter(integer -> integer % 2 == 0)
+                .collect(Collectors.toList());
+        Integer sum = evens.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        return (double) sum / evens.size();
+    }
 }
