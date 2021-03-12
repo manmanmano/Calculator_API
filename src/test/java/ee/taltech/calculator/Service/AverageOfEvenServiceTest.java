c3f7618cb4ed0322ac7d7a319cce46d95724207f
Mariano D'Angelo <madang@ttu.ee>
2021-03-12 17:30:10 +0200

    added two tests to AverageOfEvenService

--- src/test/java/ee/taltech/calculator/Service/AverageOfEvenServiceTest.java
+++ src/test/java/ee/taltech/calculator/Service/AverageOfEvenServiceTest.java
@@ -1,4 +1,26 @@
 package ee.taltech.calculator.Service;
 
+import ee.taltech.calculator.dto.AverageOfEven;
+import ee.taltech.calculator.service.AverageOfEvenService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AverageOfEvenServiceTest {
+
+    @Test
+    @DisplayName("AverageOfEvenService returns null on null")
+    void AverageOfEvenService_returns_null_on_null() {
+        assertNull(AverageOfEvenService.averageOfEven(null));
+    }
+
+    @Test
+    @DisplayName("AverageOfEvenService returns 0.0 on empty list")
+    void AverageOfEvenService_returns_zero_on_empty() {
+        assertNotNull(AverageOfEvenService.averageOfEven(List.of()));
+    }
+
+
 }
