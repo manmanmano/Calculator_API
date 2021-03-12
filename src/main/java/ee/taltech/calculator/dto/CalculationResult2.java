7c2a4b8294c28d3bdb498b8762d42dcbfe91641e
Mariano D'Angelo <madang@ttu.ee>
2021-03-12 22:03:11 +0200

    changed reversed variable type

--- src/main/java/ee/taltech/calculator/dto/CalculationResult2.java
+++ src/main/java/ee/taltech/calculator/dto/CalculationResult2.java
@@ -3,10 +3,12 @@ package ee.taltech.calculator.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class CalculationResult2 {
-    private Integer reversed;
+    private List<Integer> reversed;
     private Integer maxOdd;
     private Double average;
 }
