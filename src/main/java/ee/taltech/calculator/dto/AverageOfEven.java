4dd55fbcf2490d38455b5fa8045fd90edfc17004
Mariano D'Angelo <madang@ttu.ee>
2021-03-12 12:32:03 +0200

    added list numbers to AverageOfEven dto

--- src/main/java/ee/taltech/calculator/dto/AverageOfEven.java
+++ src/main/java/ee/taltech/calculator/dto/AverageOfEven.java
@@ -3,7 +3,10 @@ package ee.taltech.calculator.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class AverageOfEven {
+    private List<Integer> numbers;
 }
