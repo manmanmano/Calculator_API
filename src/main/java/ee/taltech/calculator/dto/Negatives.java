ec0922e2ba2fbd47362ed2a69c7591dc38fdb8e3
Mariano D'Angelo <madang@ttu.ee>
2021-03-12 12:29:58 +0200

    added list to negatives dto

--- src/main/java/ee/taltech/calculator/dto/Negatives.java
+++ src/main/java/ee/taltech/calculator/dto/Negatives.java
@@ -3,8 +3,10 @@ package ee.taltech.calculator.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class Negatives {
-
+    private List<Integer> numbers;
 }
