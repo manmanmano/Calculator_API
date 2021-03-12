c77631191f41d2f8b9f5814195fe0a608144d3bf
Mariano D'Angelo <madang@ttu.ee>
2021-03-12 12:51:08 +0200

    added logic to NegativesService

--- src/main/java/ee/taltech/calculator/service/AverageOfEvenService.java
+++ src/main/java/ee/taltech/calculator/service/AverageOfEvenService.java
@@ -1,4 +1,18 @@
 package ee.taltech.calculator.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class AverageOfEvenService {
+    public static List<Integer> findNegatives(List<Integer> numbers) {
+        if (numbers == null) {
+            return null;
+        }
+        if (numbers.isEmpty()) {
+            return List.of();
+        }
+        return numbers.stream()
+                .filter(integer -> integer < 0)
+                .collect(Collectors.toList());
+    }
 }
