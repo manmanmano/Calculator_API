b4538d341d715de0e9635ff7e4c01562b330b8a4
Jan Markus Üprus <73178486+janyprus@users.noreply.github.com>
2021-03-12 12:24:53 +0200

    answer question1

--- src/main/java/ee/taltech/a_theory2/question1/Gradle.java
+++ src/main/java/ee/taltech/a_theory2/question1/Gradle.java
@@ -8,20 +8,20 @@ public class Gradle {
     //todo A
     // In your words (do not copy wiki definitions)
     // What is gradle?
-    // Answer:
+    // Answer: A tool to build programs.
 
     //todo B
     // What is gradle wrapper? Why are we using it instead of gradle?
-    // Answer:
+    // Answer: A wrapper initiates the gradle. We are using it so we don't have to manually initiate gradle every time.
 
     //todo C
     // Name 3 different benefits or reasons for using gradle
-    // 1
-    // 2
-    // 3
+    // 1 It supports using IDE's.
+    // 2 Runs the programs that we create.
+    // 3 Shows errors and mistakes in the code.
 
     //todo D
     // Name at least 1 alternative to gradle
-    // 1
+    // 1 Maven
 
 }
