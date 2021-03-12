ea9f693af83266c75b502a3b63dfa8dd4ffc47a1
Jan Markus Üprus <73178486+janyprus@users.noreply.github.com>
2021-03-12 12:35:14 +0200

    add dto for SignReversed

--- src/main/java/ee/taltech/calculator/dto/SignReversed.java
+++ src/main/java/ee/taltech/calculator/dto/SignReversed.java
@@ -1,4 +1,12 @@
 package ee.taltech.calculator.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class SignReversed {
+    private List<Integer> numbers;
 }
