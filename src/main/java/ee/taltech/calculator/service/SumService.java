b4feb4f19ec7a76751ee307905dc14eeebf86459
Andre Tomingas <antomi@ttu.ee>
2021-03-14 17:59:53 +0200

    improved SumService

--- src/main/java/ee/taltech/calculator/service/SumService.java
+++ src/main/java/ee/taltech/calculator/service/SumService.java
@@ -1,9 +1,16 @@
 package ee.taltech.calculator.service;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
+
+@Service
 
 public class SumService {
     public static Integer Sum(List<Integer> numbers){
+        if (numbers == null || numbers.isEmpty() ){
+            return null;
+        }
         return numbers.stream()
                 .reduce(0, Integer::sum);
     }
