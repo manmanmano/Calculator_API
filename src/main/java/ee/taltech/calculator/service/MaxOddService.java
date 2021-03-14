15e99a9d87dd2be2a5b64345ce68a9dbdf939cf0
Andre Tomingas <antomi@ttu.ee>
2021-03-14 20:43:23 +0200

    improved MaxOddService

--- src/main/java/ee/taltech/calculator/service/MaxOddService.java
+++ src/main/java/ee/taltech/calculator/service/MaxOddService.java
@@ -1,12 +1,19 @@
 package ee.taltech.calculator.service;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
+
+@Service
 
 public class MaxOddService {
     public static Integer MaxOdd(List<Integer> numbers){
+        if (numbers == null || numbers.isEmpty()){
+            return null;
+        }
         return numbers.stream()
                 .filter(x -> x % 2 != 0)
                 .max(Integer::compareTo)
-                .orElse(0);
+                .orElse(null);
     }
 }
