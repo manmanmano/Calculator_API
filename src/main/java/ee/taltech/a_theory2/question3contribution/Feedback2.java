ee9f10a50a8866f5a823627f0c059ba94ab3a8fb
Andre Tomingas <antomi@ttu.ee>
2021-03-14 20:44:18 +0200

    Feedback2 done

--- src/main/java/ee/taltech/a_theory2/question3contribution/Feedback2.java
+++ src/main/java/ee/taltech/a_theory2/question3contribution/Feedback2.java
@@ -10,21 +10,21 @@ public class Feedback2 {
 
     //todo A
     // On the scale 1 to 10 how useless or useful this course was with 1 being very useless and 10 being very useful?
-    //
+    // 10. Because this course is the first time I'm touching Java and I now know it to some level.
 
     //todo B
     // Write some things you liked? At least 1, but you can write more.
-    //
-    //
+    // I liked the commentary on lesson videos, kinda felt like watching a YouTube video.
+    // I liked that there were team projects not homeworks after each lesson.
 
     //todo C
     // Write some things you didn't like? At least 1, but you can write more.
-    //
-    //
+    // Distance learning sometimes makes me lazier so I might not pay that much attention to the video, so maybe
+    // adding small homeworks would change that. Although I mentioned that I liked there were no homeworks.
 
     //todo D
     // Anything else to add? Comments? What to do differently next year? (optional)
-    //
+    // I liked this course and I can't really think of something to change in the format (distance learning) we had it.
     //
 
 }
