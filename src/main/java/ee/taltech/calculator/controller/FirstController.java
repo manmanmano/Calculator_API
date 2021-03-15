3d5e9450ddb0ac1422dc3d9a8a35058f55a991c3
Jan Markus Üprus <juprus@ttu.ee>
2021-03-15 09:41:26 +0000

    add signreverse function to calculator

--- src/main/java/ee/taltech/calculator/controller/FirstController.java
+++ src/main/java/ee/taltech/calculator/controller/FirstController.java
@@ -4,6 +4,7 @@ import ee.taltech.calculator.dto.CalculationResult1;
 import ee.taltech.calculator.dto.CalculationResult2;
 import ee.taltech.calculator.service.AverageOfEvenService;
 import ee.taltech.calculator.service.NegativesService;
+import ee.taltech.calculator.service.SignReversedService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
