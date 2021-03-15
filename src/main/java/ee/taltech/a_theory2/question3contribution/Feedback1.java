05d6cfc2523d884078a4ebaa5b2685df119dc90e
Jan Markus Üprus <juprus@ttu.ee>
2021-03-15 09:38:56 +0000

    answer feedback

--- src/main/java/ee/taltech/a_theory2/question3contribution/Feedback1.java
+++ src/main/java/ee/taltech/a_theory2/question3contribution/Feedback1.java
@@ -2,7 +2,7 @@ package ee.taltech.a_theory2.question3contribution;
 
 public class Feedback1 {
 
-    // todo this is contribution question
+ // todo this is contribution question
     //  this assignment is only for one team member (if this code is not committed by that team member how do I know this code was theirs?)
 
     // todo
@@ -10,21 +10,21 @@ public class Feedback1 {
 
     //todo A
     // On the scale 1 to 10 how useless or useful this course was with 1 being very useless and 10 being very useful?
-    //
+    // 10
 
     //todo B
     // Write some things you liked? At least 1, but you can write more.
-    //
-    //
+    // I liked the interactive way of teaching, meaning that we had to code to understand
+    // I also liked the cool and chill attitude of the teacher
 
     //todo C
     // Write some things you didn't like? At least 1, but you can write more.
-    //
+    // I did not like how fast sometimes the teacher was going, since I had to pause and rewind a couple of times
     //
 
     //todo D
     // Anything else to add? Comments? What to do differently next year? (optional)
-    //
+    // Overall the mood during the lectures was great and I really liked that
     //
 
 }
