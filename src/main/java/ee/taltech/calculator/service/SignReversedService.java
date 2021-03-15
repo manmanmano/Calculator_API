a4fcfa18078648c57097fb12e2469c0f8eb8dae3
Jan Markus Üprus <juprus@ttu.ee>
2021-03-15 09:48:56 +0000

    fix null or empty list condition

--- src/main/java/ee/taltech/calculator/service/SignReversedService.java
+++ src/main/java/ee/taltech/calculator/service/SignReversedService.java
@@ -8,11 +8,8 @@ import java.util.stream.Collectors;
 @Service
 public class SignReversedService {
     public static List<Integer> signReversed(List<Integer> numbers) {
-        if (numbers == null) {
-            throw new IllegalArgumentException("invalid input");
-        }
-        if (numbers.isEmpty()) {
-            return List.of();
+        if (numbers == null || numbers.isEmpty()) {
+            return null;
         }
         return numbers.stream()
                 .map(integer -> -1 * integer)
